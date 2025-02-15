6ad9ce224316436be060a9651e97d7c9f4af0d05
Karthiswaran-R <karthi28974@gmail.com>
2025-02-15 21:19:23 +0530

    Update Day8_mm.v

--- Day8_mm.v
+++ Day8_mm.v
@@ -1,1 +1,9 @@
-
+// D_flipflop Module Declaration
+module d_ff(q,d,clk);
+//port declaration
+input d,clk;
+output q;
+//D Flipflop functionality assign using dataflow modeling
+assign q=d;
+//End of the module
+endmodule
