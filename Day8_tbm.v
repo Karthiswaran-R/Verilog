90c844732d00e118f935c4a9ac89d47f8ee1592e
Karthiswaran-R <karthi28974@gmail.com>
2025-02-15 21:20:01 +0530

    Update Day8_tbm.v

--- Day8_tbm.v
+++ Day8_tbm.v
@@ -1,1 +1,25 @@
-
+//Test Bench Module_Declaration
+module tb_d_ff;
+//port declaration
+reg d,clk;
+wire q;
+//Module Instantiation
+d_ff dut(.q(q),.d(d),.clk(clk));
+//Test Cases
+initial begin
+	clk=1'b1;
+	#5 d=1'b0;
+	#5 d=1'b1;
+	#10 $finish;
+end
+//To Monitor the input & output
+initial begin
+	$monitor("d=%b  |  q=%b",d,q);
+end
+//To Generate the waveform
+initial begin
+	$dumpfile("tb_d_ff.vcd");
+	$dumpvars(0,tb_d_ff);
+end
+//End of the Module
+endmodule
